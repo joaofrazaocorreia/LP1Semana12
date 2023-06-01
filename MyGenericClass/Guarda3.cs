924b5f52af7c4abf13daebce7857ea50992ca7ab
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-06-01 15:03:27 +0100

    Add constructor to class Guarda3

--- MyGenericClass/Guarda3.cs
+++ MyGenericClass/Guarda3.cs
@@ -7,6 +7,13 @@ namespace MyGenericClass
 {
     public class Guarda3<T>
     {
-        
+        private T var1, var2, var3;
+
+        public Guarda3()
+        {
+            var1 = default;
+            var2 = default;
+            var3 = default;
+        }
     }
 }
