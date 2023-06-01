0a8a3bae2364121645a717b4ffc1ea2f986acf42
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-06-01 14:46:36 +0100

    Create method GetTheOnesBefore

--- TheyCameBefore/BeforeUtils.cs
+++ TheyCameBefore/BeforeUtils.cs
@@ -5,8 +5,16 @@ using System.Threading.Tasks;
 
 namespace TheyCameBefore
 {
-    public class BeforeUtils
+    public static class BeforeUtils
     {
-        
+        public static IEnumerable<T> GetTheOnesBefore(IEnumerable<T> list, T limit)
+        where T : IComparable<T> where T : struct
+        {
+            foreach (T item in list)
+            {
+                if (item > limit)
+                    yield return item;
+            }
+        }
     }
 }
