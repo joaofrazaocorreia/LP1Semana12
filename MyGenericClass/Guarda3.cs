00ddfff860f2b7538defe7322e3413298b502184
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-06-01 15:24:26 +0100

    Fix index error in Guarda3

--- MyGenericClass/Guarda3.cs
+++ MyGenericClass/Guarda3.cs
@@ -8,34 +8,30 @@ namespace MyGenericClass
     public class Guarda3<T>
     {
         private T var1, var2, var3;
+        private List<T> varList = new List<T>();
 
         public Guarda3()
         {
             var1 = default;
             var2 = default;
             var3 = default;
-        }
 
+            varList.Add(var1);
+            varList.Add(var2);
+            varList.Add(var3);
+        }
         public T GetItem(int i)
         {
-            switch (i)
-            {
-                case 0: return var1;
-                case 1: return var2;
-                case 2: return var3;
-                default: return default;
-            }
+            return varList[i];
         }
 
         public void SetItem(int i, T item)
         {
-            switch (i)
-            {
-                case 0: var1 = item;
-                case 1: var2 = item;
-                case 2: var3 = item;
-                default: throw new IndexOutOfRangeException();
-            }
+            if (i>2 || i<0)
+                throw new IndexOutOfRangeException();
+                
+            else
+                varList[i] = item;
         }
     }
 }
